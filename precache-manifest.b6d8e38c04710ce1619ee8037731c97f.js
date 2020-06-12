6d9063727b22198f3aab79e3fff79306007db806
Ashish <ashishmp0471@gmail.com>
2020-06-12 16:06:01 +0530

    Updates

---
+++ precache-manifest.b6d8e38c04710ce1619ee8037731c97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21658ab247bef44571466f9a1617c074",
+    "url": "/ctrl-c-web/index.html"
+  },
+  {
+    "revision": "2a63188bd0d73ef81074",
+    "url": "/ctrl-c-web/static/css/main.713da782.chunk.css"
+  },
+  {
+    "revision": "a9a82fc5ea357747f569",
+    "url": "/ctrl-c-web/static/js/2.49473c03.chunk.js"
+  },
+  {
+    "revision": "0c76e622f37ea53f081a70d5bb1ff958",
+    "url": "/ctrl-c-web/static/js/2.49473c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a63188bd0d73ef81074",
+    "url": "/ctrl-c-web/static/js/main.7a474943.chunk.js"
+  },
+  {
+    "revision": "0fa679021dfdf86567f4",
+    "url": "/ctrl-c-web/static/js/runtime-main.309b53ed.js"
+  }
+]);
